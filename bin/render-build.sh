7fc2d59f9af44298648e542708be717a7651988a
Alvaro Padilla <varoboss.ap@gmail.com>
2025-03-03 23:33:11 -0600

    Change render config deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+# Add to render-build.sh
+bundle exec rails runner "ActiveRecord::Base.connection.execute('SELECT 1')"
